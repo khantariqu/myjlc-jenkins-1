29d2c80cf246a7ab612e1c423bacead56bfb3a95
Khan Tarique <97331285+khantariqu@users.noreply.github.com>
2024-03-03 13:02:11 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello{
 public static void main(String args[]){
 System.out.println("printing hello jenkins");
   System.out.println("update 1 by user");
+  System.out.println("update 2 by user");
 }
 }
